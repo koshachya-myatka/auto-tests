14ae7e64b783666273700335a49514d0d2a9e813
koshachya-myatka <annaparamonova18122004@gmail.com>
2025-04-09 10:33:22 +0400

    add test

---
+++ src/test/java/ru/paramonova/AutoTestsApp/SimpleTest.java
@@ -0,0 +1,12 @@
+package ru.paramonova.AutoTestsApp;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void test() {
+        assertEquals(18, 18);
+    }
+}
